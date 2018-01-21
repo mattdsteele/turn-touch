b74529339b35224a2580a385bc7c4cc62746cd28
Matt Steele <orphum@gmail.com>
2018-01-20 22:44:24 -0600

    UX

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -11,28 +11,43 @@ export class MyApp {
   strip: TurnTouch;
   @Element() el: HTMLElement;
   @State() connected = false;
+  @State() connecting = false;
 
   connect = async () => {
     const strip = (this.el.querySelector("turn-touch") as any) as TurnTouch;
-    await strip.connect();
-    this.connected = true;
-    this.strip = strip;
+    this.connecting = true;
+    try {
+      await strip.connect();
+      this.strip = strip;
+      this.connected = true;
+    } catch (e) {
+      console.error(e);
+    } finally {
+      this.connecting = false;
+    }
   };
   get welcome() {
     return (
       <nav>
-        <button onClick={this.connect}>Connect</button>
+        {this.connecting ? (
+          <h1>Connecting...</h1>
+        ) : (
+          <button onClick={this.connect}>Connect</button>
+        )}
       </nav>
     );
   }
   @State() emittedEvents: any[] = [];
   get events() {
     return (
-      <ul>
-        {this.emittedEvents.map(e => {
-          return <li>{e}</li>;
-        })}
-      </ul>
+      <div>
+        <h1>Press some buttons</h1>
+        <ul>
+          {this.emittedEvents.map(e => {
+            return <li>{e}</li>;
+          })}
+        </ul>
+      </div>
     );
   }
 
